e4a5aa9aee88d3b1c45565e7a8ea068c283b7b8b
govher <67546073+govher-s@users.noreply.github.com>
2024-10-18 12:33:13 -0500

    Update ff_toplevel.sv

--- ff_toplevel.sv
+++ ff_toplevel.sv
@@ -1,12 +1,12 @@
 //ff_toplevel.sv
 
 	module ff_toplevel (
-		 input logic D, CLK50M, button,    
-		 output logic Q         
+		 input logic CLK50M, button, j ,k,    
+		 output logic Q, notQ         
 	);
 
 		 logic debounced_CLK;
-		 logic notQ;
+		 //logic notQ;
 		 
 		 debouncer deb (
 			  .A_noisy(button),   
@@ -14,11 +14,29 @@
 			  .A(debounced_CLK)
 		 );
 
-		 d_ff flip_flop (
+		/* d_ff flip_flop (
 			  .D(D),         
 			  .CLK(debounced_CLK),
 			  .Q(Q),             
 			  .notQ(notQ) 
+		 ); */
+		 
+		/* t_ff (
+			.T(D),
+			.CLK50M(debounced_CLK),
+			.Q(Q),
+			.notQ(notQ)
+		 
+		 ); */
+		 
+		 jk_ff(
+		 
+			.J(j),
+			.K(k),
+			.Q(Q),
+			.notQ(notQ),
+			.CLK50M(debounced_CLK)
+		 
 		 );
 
 	endmodule
